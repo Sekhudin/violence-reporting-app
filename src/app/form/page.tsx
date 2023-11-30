a2bf90e7df911d112f4da9f7208115266af857cd
sekhudin <sekhudinuap@gmail.com>
2023-11-30 21:23:36 +0700

    fix: preparation for dev link

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen py-12">
       <div>
-        <PdfCaseCreate />
+        {/* <PdfCaseCreate /> */}
       </div>
     </main>
   )
